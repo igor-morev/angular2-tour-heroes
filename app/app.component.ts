ec39a310ace4cfcb76b830e7862e6c5d6dcd8254
Igor <morev.i@mail.ru>
2016-04-15 00:13:33 +1100

    Добавил фичи в компонент app

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,11 +1,28 @@
 import {Component} from 'angular2/core';
 
+export class Hero {
+	id: number;
+	name: string;
+}
+
 @Component({
     selector: 'my-app',
-    template: '<h1>{{title}}</h1><h2>{{hero}} long details</h2>'
+    template: `
+	    <h1>{{title}}</h1>
+	    <h2>{{hero.name}} details!</h2>
+	    <div><label>id: </label>{{hero.id}}</div>
+	    <div>
+	    	<label>name: </label>
+	    	<input [(ngModel)]="hero.name" placeholder="Имя"/>
+	    </div>`
 })
 
 export class AppComponent {
 	title = 'Tour of Heroes';
-	hero = 'Windstorm'
+	hero: Hero = {
+		id: 1,
+		name: 'Windstorm'
+	};
 }
+
+
