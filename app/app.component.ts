e6077b4e33e81ac06a00303d12214e6c5ac28f4d
Andrew Savelev <savelevcorr@gmail.com>
2016-04-14 10:59:52 +1000

    change name template

--- app/app.component.ts
+++ app/app.component.ts
@@ -2,6 +2,6 @@ import {Component} from 'angular2/core';
 
 @Component({
     selector: 'my-app',
-    template: '<h1>Изучение Angular 2</h1>'
+    template: '<h1>Изучение Angular 2, rest</h1>'
 })
 export class AppComponent { }
